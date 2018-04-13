b87ebdc055a2ec504456d5ddb96fd05803df48c9
Conrad Bailey <conradbailey92@gmail.com>
2018-04-12 20:15:49 -0400

    improved shell scripts with shellchecking

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ REQUIRED_PIP_PKGS="https://storage.googleapis.com/gym-retro/builds/gym_retro-0.5
 REQUIRED_GAMES="SonicTheHedgehog-Genesis SonicTheHedgehog2-Genesis SonicAndKnuckles3-Genesis"
 
 # Validate Input #
-echo -n "Testing dir argument..."
+printf '%s' "Testing dir argument..."
 if [ $# -ne 1 ] ; then
 		printf '\n%s\n%s\n' "ERROR: Invalid number of arguments" "$USAGE" 1>&2
 		exit 1
@@ -21,29 +21,29 @@ echo "good"
 
 ROM_DIR="$1"
 
-echo -n "Testing dir existence..."
+printf '%s' "Testing dir existence..."
 if [ ! -e "$ROM_DIR" ] ; then
 		printf '\n%s\n' "ERROR: path '$ROM_DIR' does not exist" 1>&2
 		exit 1
 fi
 echo "good"
 
-echo -n "Testing dir directory..."
+printf '%s' "Testing dir directory..."
 if [ ! -d "$ROM_DIR" ] ; then
 		printf '\n%s\n' "ERROR: path '$ROM_DIR' is not a directory" 1>&2
 		exit 1
 fi
 echo "good"
 
-echo -n "Testing dir permissions..."
-if [ ! -r "$ROM_DIR" -o ! -x "$ROM_DIR" ] ; then
+printf '%s' "Testing dir permissions..."
+if [ ! -r "$ROM_DIR" ] || [ ! -x "$ROM_DIR" ] ; then
 		printf '\n%s\n' "ERROR: path '$ROM_DIR' does not have correct permissions" 1>&2
 		exit 1
 fi
 echo "good"
 
 # Gym Retro requires 64-bit architecture
-echo -n "Testing architecture..."
+printf '%s' "Testing architecture..."
 if [ "$(uname -m)" != "x86_64" ] ; then
 		printf '\n%s\n' "ERROR: only x86_64 is supported" 1>&2
 		exit 1
@@ -53,7 +53,7 @@ echo "good"
 
 # Required Binaries
 for BIN in $REQUIRED_BINS ; do
-		echo -n "Testing $BIN..."
+		printf '%s' "Testing $BIN..."
 		if [ -z $(which "$BIN" 2>/dev/null) ] ; then
 				printf '\n%s\n' "ERROR: $BIN not in path" 1>&2
 				exit 1
@@ -66,14 +66,14 @@ done
 ## Python ##
 PYTHON_REQUIRED_VERSION=360
 PYTHON_VERSION=$("$PYTHON" --version | cut -f2 -d' ' | tr -d .)
-echo -n "Testing python version exit status..."
+printf '%s' "Testing python version exit status..."
 if [ $? -ne 0 ] ; then
 		printf '\n%s\n' "ERROR: problem executing python --version" 1>&2
 		exit 1
 fi
 echo "good"
 
-echo -n "Testing python version..."
+printf '%s' "Testing python version..."
 if [ $PYTHON_VERSION -lt $PYTHON_REQUIRED_VERSION ] ; then
 		printf '\n%s\n' "ERROR: $PYTHON must be of version $PYTHON_REQUIRED_VERSION" 1>&2
 		exit 1
@@ -82,21 +82,21 @@ echo "good"
 
 ## glibc ##
 LIBC=$(ldd --version | head -n1 | awk -F'[()]' '{print $2}')
-echo -n "Testing ldd exit status..."
+printf '%s' "Testing ldd exit status..."
 if [ $? -ne 0 ] ; then
 		printf '\n%s\n' "ERROR: problem executing ldd" 1>&2
 		exit 1
 fi
 echo "good"
 
-echo -n "Testing glibc existence..."
+printf '%s' "Testing glibc existence..."
 if [ "$LIBC" != "GNU libc" ] ; then
 		printf '\n%s\n' "ERROR: GNU libc $GLIBC_REQUIRED_VERSION is required" 1>&2
 		exit 1
 fi
 echo "good"
 
-echo -n "Testing ldd exit status..."
+printf '%s' "Testing ldd exit status..."
 GLIBC_REQUIRED_VERSION=214
 GLIBC_VERSION=$(ldd --version | head -n1 | sed 's/ /\n/g' | tail -1 | tr -d .)
 if [ $? -ne 0 ] ; then
@@ -105,7 +105,7 @@ if [ $? -ne 0 ] ; then
 fi
 echo "good"
 
-echo -n "Testing glibc version..."
+printf '%s' "Testing glibc version..."
 if [ $GLIBC_VERSION -lt $GLIBC_REQUIRED_VERSION ] ; then
 		printf '\n%s\n' "ERROR: GNU libc $GLIBC_REQUIRED_VERSION is required" 1>&2
 		exit 1
@@ -113,7 +113,7 @@ fi
 echo "good"
 
 # Setup Virtual Environment #
-echo -n "Making venv..."
+printf '%s' "Making venv..."
 OUTPUT=$(virtualenv -p"$PYTHON" 2>&1 venv)
 STATUS=$?
 if [ $STATUS -ne 0 ] ; then
@@ -122,7 +122,7 @@ if [ $STATUS -ne 0 ] ; then
 fi
 echo "good"
 
-echo -n "Activating venv..."
+printf '%s' "Activating venv..."
 . venv/bin/activate
 STATUS=$?
 if [ $STATUS -ne 0 ] ; then
@@ -132,7 +132,7 @@ fi
 echo "good"
 
 ## Retrieve Contest Version of Gym ##
-echo -n "Cloning contest repo..."
+printf '%s' "Cloning contest repo..."
 OUTPUT=$(git clone -q --recursive https://github.com/openai/retro-contest.git)
 STATUS=$?
 if [ $STATUS -ne 0 ] ; then
@@ -142,7 +142,7 @@ fi
 echo "good"
 
 ## Pip Install Contest Version of Gym ##
-echo -n "Installing contest pip package..."
+printf '%s' "Installing contest pip package..."
 OUTPUT=$("$PIP" install -e 'retro-contest/support[docker]')
 STATUS=$?
 if [ $STATUS -ne 0 ] ; then
@@ -153,7 +153,7 @@ echo "good"
 
 ## Pip Install Non-Local Packages ##
 for PKG in $REQUIRED_PIP_PKGS ; do
-		echo -n "Installing pip pkg $PKG..."
+		printf '%s' "Installing pip pkg $PKG..."
 		OUTPUT=$("$PIP" install "$PKG" 2>&1)
 		STATUS=$?
 		if [ $STATUS -ne 0 ] ; then
@@ -165,12 +165,12 @@ done
 
 
 # Install ROMs into Retro Gym #
-echo -n "Importing games..."
+printf '%s' "Importing games..."
 OUTPUT=$("$PYTHON" -m retro.import "$ROM_DIR")
 echo "good"
 
 for GAME in $REQUIRED_GAMES ; do
-		echo -n "Finding game $GAME..."
+		printf '%s' "Finding game $GAME..."
 		if [ $(echo "$OUTPUT" | grep -c "$GAME") -eq 0 ] ; then
 				printf '\n  %s\n' "WARNING: Did not find '$GAME' in ROM directory '$ROM_DIR'" 1>&2
 		else
