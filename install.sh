553502d7bb1f70ec30e36976e93d463e97c2c50c
Conrad Bailey <conradbailey92@gmail.com>
2018-04-24 01:15:55 -0400

    Added analyze_monitor.py dependencies to installation procedure

--- install.sh
+++ install.sh
@@ -25,6 +25,8 @@ REQUIRED_BINS="$PIP $PYTHON grep virtualenv git"
 REQUIRED_PIP_PKGS="gym-retro \
                    ipykernel \
 									 numpy \
+									 pandas \
+									 matplotlib \
 									"
 
 REQUIRED_GAMES="SonicTheHedgehog-Genesis SonicTheHedgehog2-Genesis SonicAndKnuckles3-Genesis"
